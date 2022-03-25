6592a65e3d21c4c9de80be8b633266d28e8b9121
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-25 02:26:40 -0600

    Aplicando ngOnDrestroy

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -1,4 +1,5 @@
 import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pagina1',
@@ -8,8 +9,10 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 })
 export class Pagina1Component implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, 
                  AfterViewInit, AfterViewChecked, OnDestroy {
-                   
-  nombre: string = 'Fernando';
+
+  nombre           : string = 'Fernando';
+  segundos         : number = 0;
+  timerSubscripton!: Subscription;
 
   /* Se ejecuta una nueva instacia y la ejecuta Angular */
   constructor() { 
@@ -19,6 +22,9 @@ export class Pagina1Component implements OnInit, OnChanges, DoCheck, AfterConten
   /* Cuando el componente es creado */
   ngOnInit(): void {
     console.log('ngOnInit');
+    this.timerSubscripton = interval(1000).subscribe( i => {
+      this.segundos = i;
+    })
   }
   
   ngOnChanges(changes: SimpleChanges): void {
@@ -47,9 +53,9 @@ export class Pagina1Component implements OnInit, OnChanges, DoCheck, AfterConten
 
   ngOnDestroy(): void {
     console.log('ngOnDestroy');
+    this.timerSubscripton.unsubscribe();
+    console.log('timer limpiado');
   }
 
-  guardar() {
-
-  }
+  guardar() { }
 }
